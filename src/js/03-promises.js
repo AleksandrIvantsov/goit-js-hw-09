a6f8b8febbca616880288a34c44bdd44eb331a47
Aleksandr Ivantsov <7022713@gmail.com>
2023-03-15 23:43:11 +0200

    Promis

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,26 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', onButtonSubmitClick);
+console.log(form);
+
+function onButtonSubmitClick(event) {
+  event.preventDefault();
+  // for (let i = 1; i < clients.length; i += 1) {
+  //   console.log(clients[i]);
+  // }
+  console.log(event);
 }
+
+// function createPromise(position, delay) {
+
+//   return new Promise((resolve, reject) => {
+//     const shouldResolve = Math.random() > 0.3;
+
+//     if (shouldResolve) {
+//       // Fulfill
+//     } else {
+//       // Reject
+//     }
+//   });
+// }
